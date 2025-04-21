ffcf0f3130b0c1e13f087aa7c1d3df6c908f2f97
waled-x <waled44x44@gmail.com>
2025-04-21 19:55:08 +0300

    حذف الملفات القديمة لإعادة رفع المشروع بشكل مرتب

--- First release/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = new mysqli("localhost", "root", "", "college_maintenance_fixed");
-    if ($conn->connect_error) {
-        die("فشل الاتصال: " . $conn->connect_error);
-
-    }
-    
-?>
